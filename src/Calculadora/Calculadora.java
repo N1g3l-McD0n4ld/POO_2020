62ea43986ccdd6688b3c8c9a3068871983ae4185
NEC <NEC@VR>
2020-09-27 19:42:31 -0300

    Modulo calculadora.

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,15 @@
+package Calculadora;
+
+import java.util.ArrayList;
+
+public class Calculadora {
+	private ArrayList<String> historial;
+	
+	private void saveToHistory(String operacion) {
+		historial.add(operacion);
+	}
+	public double suma(double a, double b) {
+		saveToHistory(a+"+"+b);
+		return a+b;
+		}
+}
